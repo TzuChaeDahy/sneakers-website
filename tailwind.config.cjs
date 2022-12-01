84f5ec49d90aa9d1471b6208864568f1b77a8e7b
TzuChaeDahy <viniciuslovestwice@gmail.com>
2022-11-30 21:34:53 -0300

    commiting first files to GitHub

---
+++ tailwind.config.cjs
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{jsx,js}"],
+  theme: {
+    extend: {
+      colors: {
+        project: {
+          orange: {
+            100: "hsl(25, 100%, 94%)",
+            200: "hsl(26, 100%, 55%)",
+          },
+          black: {
+            100: "hsl(220, 14%, 75%)",
+            200: "hsl(219, 9%, 45%)",
+            300: "hsl(220, 13%, 13%)",
+            400: "hsl(0, 0%, 0%)",
+          },
+          white: {
+            100: "hsl(0, 0%, 100%)",
+            200: "hsl(223, 64%, 98%)",
+          },
+        },
+      },
+      fontFamily: {
+        kumbh: ["'Kumbh Sans'", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
